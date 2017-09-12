07c5e0574feef0524a38feaa22be8ff8b11bd084
xiaoliufu <xiaoliufu@meituan.com>
2017-09-12 22:54:35 +0800

    完成第一步

---
+++ src/main/java/com/example/l01/AkkaDb.java
@@ -0,0 +1,30 @@
+package com.example.l01;
+
+import akka.actor.AbstractActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * author: xiaoliufu
+ * date:   2017/9/7.
+ * description:
+ */
+public class AkkaDb extends AbstractActor {
+    protected final LoggingAdapter log = Logging.getLogger(context().system(), this);
+    protected final Map<String, Object> map = new HashMap<>();
+
+    private AkkaDb() {
+        receive(ReceiveBuilder
+                .match(SetRequest.class, message -> {
+                    log.info("Received Set request: {}", message);
+                })
+                .matchAny(o -> log.info("received unknown message: {}", o)).build()
+        );
+
+
+    }
+}
---
+++ src/main/java/com/example/l01/SetRequest.java
@@ -0,0 +1,24 @@
+package com.example.l01;
+
+/**
+ * author: xiaoliufu
+ * date:   2017/9/7.
+ * description:
+ */
+public class SetRequest {
+    private final String key;
+    private final Object value;
+
+    public SetRequest(String key, Object value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
